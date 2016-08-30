8d0d82b7cb7d695b1f2d996428ab42dbfd14aa88
616764899 <a616764899@126.com>
2016-08-30 20:24:57 +0800

    添加弹出界面的按钮

--- app/src/main/java/com/meimei/processdemo/LockActivity.java
+++ app/src/main/java/com/meimei/processdemo/LockActivity.java
@@ -1,7 +1,10 @@
 package com.meimei.processdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  *  随便一个用于跳转的界面
@@ -12,5 +15,27 @@ public class LockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
+        Button button = (Button) findViewById(R.id.bt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+                MyIntent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(MyIntent);
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+        MyIntent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(MyIntent);
+
+        finish();
     }
 }
